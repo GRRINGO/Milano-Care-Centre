3bd1ede25135b8200a7a1b5c48f147b7e303504c
Noah <noahakai.nettey@mail.polimi.it>
2018-06-19 12:32:13 +0200

    Changed db to postgres.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function initSqlDB() {
     process.env.TEST=false// node ./index.js
 
 
-    if (process.env.TEST) {
+    if (false) {
         sqlDb = sqlDbFactory({
             client: "sqlite3",
             debug: true,
